067e4fd3a8ffee04747050e74bb61487f3682d19
anil-patro <anil.patro@bigbasket.com>
2022-02-28 17:11:00 +0530

    Established the unidirectional OnetoOne relation between Ingredient and UnitOfMeasure class

--- src/main/java/com/example/spring5receipeapp/domain/Ingredient.java
+++ src/main/java/com/example/spring5receipeapp/domain/Ingredient.java
@@ -12,6 +12,8 @@ public class Ingredient {
     private BigDecimal amount;
     @ManyToOne
     private Recipe recipe;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     public Long getId() {
         return id;
@@ -45,5 +47,11 @@ public class Ingredient {
         this.recipe = recipe;
     }
 
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
 
+    public void setUom(UnitOfMeasure unitOfMeasure) {
+        this.uom = unitOfMeasure;
+    }
 }
