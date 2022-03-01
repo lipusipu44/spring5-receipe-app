98159c1ab9bbd056041d6e46df5ad484c0b31527
anil-patro <anil.patro@bigbasket.com>
2022-03-01 08:56:30 +0530

    Added the findBy methods to Category and UOM repository interface and called them in controller class

--- src/main/java/com/example/spring5receipeapp/controller/IndexController.java
+++ src/main/java/com/example/spring5receipeapp/controller/IndexController.java
@@ -1,13 +1,38 @@
 package com.example.spring5receipeapp.controller;
 
+import com.example.spring5receipeapp.domain.Category;
+import com.example.spring5receipeapp.domain.UnitOfMeasure;
+import com.example.spring5receipeapp.repository.CategoryRepository;
+import com.example.spring5receipeapp.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"/","","/index"})
     public String getIndexPage(){
+        Optional<Category> mexican = categoryRepository.findByDescription("Mexican");
+        System.out.println("category by id:"+mexican.get().getId());
+
+        Optional<Category> byId = categoryRepository.findById(1L);
+        System.out.println("category found by id:"+byId.get().getDescription());
+
+        Optional<UnitOfMeasure> pinch = unitOfMeasureRepository.findByDescription("Pinch");
+        System.out.println("Uom is:"+pinch.get().getId());
+
+
+
         return "index";
     }
 }
--- src/main/java/com/example/spring5receipeapp/repository/CategoryRepository.java
+++ src/main/java/com/example/spring5receipeapp/repository/CategoryRepository.java
@@ -3,5 +3,10 @@ package com.example.spring5receipeapp.repository;
 import com.example.spring5receipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category,Long> {
+
+    Optional<Category> findByDescription(String description);
+    Optional<Category> findById(Long id);
 }
