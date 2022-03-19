049f73ec599ac97f01edb5d786eddf9a4db5ee5d
anil-patro <anil.patro@bigbasket.com>
2022-03-19 19:50:45 +0530

    Added first Junit class and test

---
+++ src/test/java/com/example/spring5receipeapp/domain/CategoryTest.java
@@ -0,0 +1,31 @@
+package com.example.spring5receipeapp.domain;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class CategoryTest {
+    Category category;
+    @Before
+    public void setUp(){
+        category=new Category();
+    }
+
+    @Test
+    public void getId() {
+        Long idVal=4l;
+        category.setId(idVal);
+        assertEquals(idVal,category.getId());
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
