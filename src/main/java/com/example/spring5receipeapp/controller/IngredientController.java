2eb584967cfe3b2b93c8c266973877aafb6b4dc8
anil-patro <anil.patro@bigbasket.com>
2022-03-29 18:35:06 +0530

    Added thymleaf files and controllers for ingredient view

---
+++ src/main/java/com/example/spring5receipeapp/controller/IngredientController.java
@@ -0,0 +1,27 @@
+package com.example.spring5receipeapp.controller;
+
+import com.example.spring5receipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IngredientController {
+
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+    @GetMapping
+    @RequestMapping("/recipe/{recipeId}/ingredients")
+    public String listIngredients(@PathVariable String recipeId, Model model){
+        log.info("Getting Ingredient list for recipe id:"+recipeId);
+        model.addAttribute("recipe",recipeService.findCommandById(Long.valueOf(recipeId)));
+        return "recipe/ingredient/list";
+    }
+}
