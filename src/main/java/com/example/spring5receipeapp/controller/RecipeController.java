9cdb2f02da168c1620a88a8be93136ab1644675a
anil-patro <anil.patro@bigbasket.com>
2022-03-29 11:16:10 +0530

    Created a recipe form, added a new recipe using POST

--- src/main/java/com/example/spring5receipeapp/controller/RecipeController.java
+++ src/main/java/com/example/spring5receipeapp/controller/RecipeController.java
@@ -1,9 +1,12 @@
 package com.example.spring5receipeapp.controller;
 
+import com.example.spring5receipeapp.command.RecipeCommand;
 import com.example.spring5receipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,4 +22,19 @@ public class RecipeController {
         model.addAttribute("recipe",recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand saveRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/"+saveRecipeCommand.getId();
+    }
+
+
 }
