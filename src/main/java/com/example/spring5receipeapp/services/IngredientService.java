5c5afd9f347d1dca88ac2d1df5f429f0e59adef7
anil-patro <anil.patro@bigbasket.com>
2022-03-30 21:40:53 +0530

    Changed classes for Ingredient deletion

--- src/main/java/com/example/spring5receipeapp/services/IngredientService.java
+++ src/main/java/com/example/spring5receipeapp/services/IngredientService.java
@@ -5,4 +5,5 @@ import com.example.spring5receipeapp.command.IngredientCommand;
 public interface IngredientService {
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
     IngredientCommand saveIngredientCommand(IngredientCommand command);
+    void deleteById(Long recipeId, Long idToDelete);
 }
