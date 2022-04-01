00cdbaeab85957e38c4abbaa7d3b892599e55ad7
anil-patro <anil.patro@bigbasket.com>
2022-04-01 11:01:38 +0530

    Added changes for Number format exception handling

--- src/main/java/com/example/spring5receipeapp/controller/RecipeController.java
+++ src/main/java/com/example/spring5receipeapp/controller/RecipeController.java
@@ -61,5 +61,15 @@ public class RecipeController {
         modelAndView.setViewName("404error");
         return modelAndView;
     }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormatException(Exception exception){
+        log.error("Handling Number format exception");
+        log.error(exception.getMessage());
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.addObject("exception",exception);
+        modelAndView.setViewName("400error");
+        return modelAndView;
+    }
 
 }
