0c9da071b2b72b01e579dbcdc0fbef7a55ff0692
anil-patro <anil.patro@bigbasket.com>
2022-04-15 00:04:30 +0530

    Segregating default h2 and mysql profile

--- src/main/java/com/example/spring5receipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/example/spring5receipeapp/bootstrap/RecipeBootstrap.java
@@ -6,6 +6,7 @@ import com.example.spring5receipeapp.repository.RecipeRepository;
 import com.example.spring5receipeapp.repository.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,7 @@ import java.util.Optional;
 
 @Component
 @Slf4j
+@Profile("default")
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
     private final CategoryRepository categoryRepository;
     private final RecipeRepository recipeRepository;
